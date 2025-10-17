c836a40147409ded282904c1ae709629172cb441
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-10-18 02:28:25 +0600

    adjusted layout one

--- src/Layouts/One.jsx
+++ src/Layouts/One.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 const One = () => {
   return (
     <div className='bg-[#EEE8AA] rounded-xl w-full h-full p-5 relative flex flex-col justify-center '>
-      <p className='font-bold text-2xl text-black'>Create and schedule content <span className='italic text-[#483D8B]'>quicker.</span></p>
+      <p className='font-bold text-2xl text-black leading-none'>Create and schedule content <span className='italic text-[#483D8B]'>quicker.</span></p>
       <button className='btn bg-[#dfb645] text-[#483D8B] border-none rounded-full font-medium text-xs mt-5 w-full'>Create Post <i class="fa-solid fa-plus"></i></button>
-      <div className='absolute bottom-9 right-22 text-black'><i class="fa-solid fa-location-arrow"></i></div>
+      <div className='absolute bottom-13 right-22 text-black'><i class="fa-solid fa-location-arrow"></i></div>
     </div>
   );
 };
