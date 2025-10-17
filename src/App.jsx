d15b2caeb08ee6e30dd390c2eda16de886fec7b0
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-10-18 02:26:29 +0600

    added layout one

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
 import './App.css'
+import One from './Layouts/one'
 
 function App() {
 
   return (
     <>
-      <div className='max-w-7xl mx-auto flex justify-center items-center h-screen'>
+      <div className='max-w-7xl mx-auto flex justify-center items-center h-screen poppins'>
         <div class="grid grid-cols-8 grid-rows-9 gap-4 h-[600px] w-[800px] relative">
-          <div class="col-span-2 row-span-5 h-[90%] bg-red-800">1</div>
-          <div class="col-span-2 row-span-4 col-start-1 row-start-6 h-[111%] absolute w-full -top-7 bg-red-400">2</div>
-          <div class="col-span-2 row-span-3 col-start-3 row-start-4 bg-red-950">3</div>
-          <div class="col-span-2 row-span-3 col-start-5 row-start-4 bg-green-900">4</div>
-          <div class="col-span-2 row-span-6 col-start-7 row-start-1 bg-green-700">5</div>
-          <div class="col-span-4 row-span-3 col-start-5 row-start-7 bg-blue-700">6</div>
-          <div class="col-span-2 row-span-3 col-start-3 row-start-7 bg-gray-800">7</div>
-          <div class="col-span-4 row-span-3 col-start-3 row-start-1 bg-purple-800">8</div>
+          <div class="col-span-2 row-span-5 h-[90%] rounded-xl bg-red-800">
+            <One></One>
+          </div>
+          <div class="col-span-2 row-span-4 col-start-1 row-start-6 h-[111%] absolute w-full -top-7 rounded-xl bg-red-400">2</div>
+          <div class="col-span-2 row-span-3 col-start-3 row-start-4 rounded-xl bg-red-950">3</div>
+          <div class="col-span-2 row-span-3 col-start-5 row-start-4 rounded-xl bg-green-900">4</div>
+          <div class="col-span-2 row-span-6 col-start-7 row-start-1 rounded-xl bg-green-700">5</div>
+          <div class="col-span-4 row-span-3 col-start-5 row-start-7 rounded-xl bg-blue-700">6</div>
+          <div class="col-span-2 row-span-3 col-start-3 row-start-7 rounded-xl bg-gray-800">7</div>
+          <div class="col-span-4 row-span-3 col-start-3 row-start-1 rounded-xl bg-purple-800">8</div>
         </div>
       </div>
 
