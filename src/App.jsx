6323c15902a3dc6198e2465115dbc2d1489a49d4
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-10-18 02:43:16 +0600

    added layout eight

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Eight from './Layouts/Eight'
 import One from './Layouts/one'
 
 function App() {
@@ -16,7 +17,9 @@ function App() {
           <div class="col-span-2 row-span-6 col-start-7 row-start-1 rounded-xl bg-green-700">5</div>
           <div class="col-span-4 row-span-3 col-start-5 row-start-7 rounded-xl bg-blue-700">6</div>
           <div class="col-span-2 row-span-3 col-start-3 row-start-7 rounded-xl bg-gray-800">7</div>
-          <div class="col-span-4 row-span-3 col-start-3 row-start-1 rounded-xl bg-purple-800">8</div>
+          <div class="col-span-4 row-span-3 col-start-3 row-start-1 rounded-xl bg-purple-800">
+            <Eight></Eight>
+          </div>
         </div>
       </div>
 
---
+++ src/Layouts/Eight.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Eight = () => {
+  return (
+    <div className='bg-[#5f31df] w-full h-full rounded-xl flex flex-col justify-center items-center'>
+      <p className='font-medium text-4xl text-center leading-none'>Social Media <span className='text-[#e0b12d]'>10x</span> <span className='italic'>Faster</span> with AI</p>
+      <div className='text-[#e0b12d] flex gap-3 py-2'>
+        <i class="fa-solid fa-star"></i>
+        <i class="fa-solid fa-star"></i>
+        <i class="fa-solid fa-star"></i>
+        <i class="fa-solid fa-star"></i>
+        <i class="fa-solid fa-star"></i>
+      </div>
+      <p className='text-xs'>Over 4,000 5-star reviews</p>
+    </div>
+  );
+};
+
+export default Eight;
