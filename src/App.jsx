26bfd975ed909ef8a715500298600cb94f409c20
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-10-18 01:54:59 +0600

    added basic structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='max-w-7xl mx-auto flex justify-center items-center h-screen'>
+        <div class="grid grid-cols-8 grid-rows-9 gap-4 h-[600px] w-[800px] relative">
+          <div class="col-span-2 row-span-5 h-[90%] bg-red-800">1</div>
+          <div class="col-span-2 row-span-4 col-start-1 row-start-6 h-[111%] absolute w-full -top-7 bg-red-400">2</div>
+          <div class="col-span-2 row-span-3 col-start-3 row-start-4 bg-red-950">3</div>
+          <div class="col-span-2 row-span-3 col-start-5 row-start-4 bg-green-900">4</div>
+          <div class="col-span-2 row-span-6 col-start-7 row-start-1 bg-green-700">5</div>
+          <div class="col-span-4 row-span-3 col-start-5 row-start-7 bg-blue-700">6</div>
+          <div class="col-span-2 row-span-3 col-start-3 row-start-7 bg-gray-800">7</div>
+          <div class="col-span-4 row-span-3 col-start-3 row-start-1 bg-purple-800">8</div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
   )
 }
